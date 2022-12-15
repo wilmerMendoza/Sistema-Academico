d4521604bc1dbe46e22ebfe4ba7fdae5c50180a8
wilmerMendoza <115171329+wilmerMendoza@users.noreply.github.com>
2022-12-15 17:00:39 -0500

    avance

--- modulo_formativo.php
+++ modulo_formativo.php
@@ -1,11 +1,12 @@
-<?php 
-include "include/conexion.php"; 
+<?php
+include "include/conexion.php";
 include "include/busquedas.php";
 include "include/verificar_sesion.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
@@ -20,88 +21,93 @@ include "include/verificar_sesion.php";
     <link href="Gentella/vendors/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
     <link href="Gentella/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-    <!-- Datatables -->
-    <link href="Gentella/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
-    <link href="Gentella/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
-    <link href="Gentella/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
-    <link href="Gentella/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
-    <link href="Gentella/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
-
+    <!-- bootstrap-progressbar -->
+    <link href="Gentella/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="Gentella/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
+    <!-- bootstrap-daterangepicker -->
+    <link href="Gentella/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
     <!-- Custom Theme Style -->
     <link href="Gentella/build/css/custom.min.css" rel="stylesheet">
-  </head>
-  <body class="nav-md">
-    <div class="container body">
-      <div class="main_container">
-        <?php include "include/menu.php" ?>  
-        <!-- Menu en la parte superior -->
-        <!-- page content -->
-        <div class="right_col" role="main">
-        <div class="row">
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Modulo Formativo</h2>
-                    <ul class="nav navbar-right">
-                      <li>
-                        <a href="modulo_formativos.php" class="btn btn-success">Agregar Nuevo</a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    
-                    <table id="example" class="table table-striped table-bordered">
-                      <thead>
-                        <tr>
-                          <th>ID</th>
-                          <th>PROGRAMA DE ESTUDIOS</th>
-                          <th>NUMERO DE MODULO </th>
-                          <th>NOMBRE</th>
-                        
+</head>
 
-                          <th>acciones</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <?php 
-                        $b_docentes = buscarDocentes($conexion);
-                        while ($res_b_docentes = mysqli_fetch_array($b_docentes)) {
-                        ?>
-                        <tr>
-                          <td><?php echo $res_b_docentes['id']; ?></td>
-                          <td><?php echo $res_b_docentes['programa_estudios']; ?></td>
-                          <td><?php echo $res_b_docentes['numero_modulo'];  ?></td>
-                          <td><?php echo $res_b_docentes['nombre'];  ?></td>
-                         
-                          <td>
-                            <a href="editar_docente.php?id=<?php echo $res_b_docentes['id']; ?>" class="btn btn-primary">Editar</a>
-                            <a href="operaciones/eliminar_docente.php?id=<?php echo $res_b_docente['id']; ?>" class="btn btn-danger">Eliminar</a>
-                          </td>
-                        </tr>
-                        <?php
-                        }
-                        ?>
-                        
-                      </tbody>
-                    </table>
-                  </div>
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+            <?php include "include/menu.php" ?>
+            <!-- Menu en la parte superior -->
+            <!-- page content -->
+            <div class="right_col" role="main">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="x_panel">
+                            <div class="x_title">
+                                <h2>Registrar Módulos Formativo</h2>
+
+                                <div class="clearfix"></div>
+                            </div>
+                            <div class="x_content">
+                                <br />
+                                <form class="form-horizontal form-label-left" method="POST" action="operaciones/resgitrar_modulo.php">
+
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Carrera Profesional :
+                                        </label>
+                                        <div class="col-md-6 col-sm-6 col-xs-12">
+                                            <select type="text" name="carrera" class="form-control col-md-7 col-xs-12">
+                                            <option value="">Seleccionar Programa</option>
+                                            <?php 
+                                            $b_prog = buscarProgramaEstudio($conexion);
+                                            while ($res_b_prog = mysqli_fetch_array($b_prog)) {
+                                            ?> 
+                                            <option value="<?php echo $res_b_prog['id']?>"> <?php echo $res_b_prog['nombre']?> </option>
+                                            <?php
+                                            }
+                                            ?>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Nombre :
+                                        </label>
+                                        <div class="col-md-6 col-sm-6 col-xs-12">
+                                            <input type="text" name="descripcion" class="form-control col-md-7 col-xs-12">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Nro de Módulo :
+                                        </label>
+                                        <div class="col-md-6 col-sm-6 col-xs-12">
+                                            <input type="text" name="nro_modulo" class="form-control col-md-7 col-xs-12">
+                                        </div>
+                                    </div>
+                            </div>
+                            <div class="ln_solid"></div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                    <a class="btn btn-warning" type="button" href="./modulo_formativo.php">Cancelar</a>
+                                    <button class="btn btn-primary" type="reset">Limpiar</button>
+                                    <button type="submit" class="btn btn-success">Guardar</button>
+                                </div>
+                            </div>
+
+                            </form>
+                        </div>
+                    </div>
                 </div>
-              </div>
-          </div>
+            </div>
         </div>
         <!-- /page content -->
         <!-- footer content -->
         <footer>
-          <div class="pull-right">
-            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-          </div>
-          <div class="clearfix"></div>
+            <div class="pull-right">
+                Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+            </div>
+            <div class="clearfix"></div>
         </footer>
         <!-- /footer content -->
-      </div>
     </div>
-    
+    </div>
     <!-- jQuery -->
     <script src="Gentella/vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
@@ -110,51 +116,37 @@ include "include/verificar_sesion.php";
     <script src="Gentella/vendors/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
     <script src="Gentella/vendors/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="Gentella/vendors/Chart.js/dist/Chart.min.js"></script>
+    <!-- gauge.js -->
+    <script src="Gentella/vendors/gauge.js/dist/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="Gentella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
     <!-- iCheck -->
     <script src="Gentella/vendors/iCheck/icheck.min.js"></script>
-    <!-- Datatables -->
-    <script src="Gentella/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="Gentella/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-    <script src="Gentella/vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="Gentella/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
-    <script src="Gentella/vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
-    <script src="Gentella/vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="Gentella/vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="Gentella/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
-    <script src="Gentella/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-    <script src="Gentella/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="Gentella/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
-    <script src="Gentella/vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
-    <script src="Gentella/vendors/jszip/dist/jszip.min.js"></script>
-    <script src="Gentella/vendors/pdfmake/build/pdfmake.min.js"></script>
-    <script src="Gentella/vendors/pdfmake/build/vfs_fonts.js"></script>
-
+    <!-- Skycons -->
+    <script src="Gentella/vendors/skycons/skycons.js"></script>
+    <!-- Flot -->
+    <script src="Gentella/vendors/Flot/jquery.flot.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.pie.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.time.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.stack.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="Gentella/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="Gentella/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="Gentella/vendors/flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="Gentella/vendors/DateJS/build/date.js"></script>
+    <!-- JQVMap -->
+    <script src="Gentella/vendors/jqvmap/dist/jquery.vmap.js"></script>
+    <script src="Gentella/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script src="Gentella/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="Gentella/vendors/moment/min/moment.min.js"></script>
+    <script src="Gentella/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
     <!-- Custom Theme Scripts -->
     <script src="Gentella/build/js/custom.min.js"></script>
-    <script>
-    $(document).ready(function() {
-    $('#example').DataTable({
-      "language":{
-    "processing": "Procesando...",
-    "lengthMenu": "Mostrar _MENU_ registros",
-    "zeroRecords": "No se encontraron resultados",
-    "emptyTable": "Ningún dato disponible en esta tabla",
-    "sInfo": "Mostrando del _START_ al _END_ de un total de _TOTAL_ registros",
-    "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-    "search": "Buscar:",
-    "infoThousands": ",",
-    "loadingRecords": "Cargando...",
-    "paginate": {
-        "first": "Primero",
-        "last": "Último",
-        "next": "Siguiente",
-        "previous": "Anterior"
-    },
-      }
-    });
+</body>
 
-    } );
-    </script>
-  </body>
 </html>
